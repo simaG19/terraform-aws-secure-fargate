9180c7094405eef6348c1a8a8f5e15b53d8ba890
Simon Wubshet Bekele <simonwubshet254@gmail.com>
2026-06-30 16:02:19 +0300

    aws ecs setup

---
+++ ecs.tf
@@ -0,0 +1,62 @@
+#Logical Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/${var.project_name}"
+  retention_in_days = 7
+}
+
+#Definition
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.project_name}-task"
+  network_mode             = "awsvpc" 
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"  
+  memory                   = "512"  
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "secure-app"
+    image     = "python:3.11-alpine" 
+    essential = true
+    
+    portMappings = [{
+      containerPort = var.container_port
+      hostPort      = var.container_port
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+        "awslogs-region"        = var.aws_region
+        "awslogs-stream-prefix" = "ecs"
+      }
+    }
+  }])
+}
+
+# ECS Service
+resource "aws_ecs_service" "main" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2 # instances across distinct AZs 
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true 
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "secure-app"
+    container_port   = var.container_port
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
